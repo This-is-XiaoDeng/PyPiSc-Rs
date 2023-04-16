d848e2cb957f6dd10ffeffc8e094002d0c25de33
This is XiaoDeng <1744793737@qq.com>
2023-04-16 10:58:33 +0800

    支持在 PyPi.org 上搜索

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use colored::Colorize;
 mod list;
 use std::env;
 use reqwest;
+mod search;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -24,10 +25,13 @@ async fn main() -> Result<(), reqwest::Error> {
     } else if args[1] == "help" {
         println!("{} list [name]: List packages by [name]", args[0]);
         println!("{} help: Show this help", args[0]);
-        println!("{} update: Update cache", args[0])
+        println!("{} update: Update cache", args[0]);
+        println!("{} search <name>: Search package on pypi.org", args[0]);
     } else if args[1] == "update" {
         println!("Updating cache ...");
         list::make_cache().await?;
+    } else if args[1] == "search" {
+        search::search_package(args[2].clone()).await.unwrap();
     }
     Ok(())
 }
---
+++ src/search.rs
@@ -0,0 +1,63 @@
+use reqwest;
+use scraper::{Html, Selector};
+use colored::Colorize;
+
+async fn get_page(name: &String, page: u16) -> Result<Html, reqwest::Error> {
+    let html = reqwest::Client::new()
+        .get(format!("https://pypi.org/search/?q={}&page={}", name, page))
+        .send()
+        .await?
+        .text()
+        .await?;
+    Ok(Html::parse_fragment(&html))  
+}
+
+fn get_packages(page: Html) -> Result<Vec<Html>, String> {
+    let selector = Selector::parse(".package-snippet").unwrap();
+    let mut package_list: Vec<Html> = Vec::new();
+    for el in page.select(&selector) {
+        let html = el.inner_html();
+        package_list.push(Html::parse_fragment(&html));
+    }
+    Ok(package_list)
+}
+
+fn get_package_data(package: &Html, data: &'static str) -> Result<String, ()> {
+    let selector = Selector::parse(format!(".package-snippet__{}", data).as_str()).unwrap();
+    for el in package.select(&selector) {
+        return Ok(el.inner_html().to_string());
+    }
+    return Err(())
+}
+
+fn get_create_time(package: &Html) -> Result<String, ()> {
+    let data = get_package_data(&package, "created").unwrap();
+    let doc = Html::parse_fragment(&data);
+    let selector = Selector::parse("time").unwrap();
+    for el in doc.select(&selector) {
+        return Ok(el.inner_html().replace("\n", "").replace("  ", ""));
+    }
+    return Err(());
+}
+
+pub async fn search_package(name: String) -> Result<(), ()> {
+    let mut p = 1;
+    loop {
+        // println!("getting page {}", p);
+        let page = get_page(&name, p).await.unwrap();
+        let packages = get_packages(page).unwrap();
+        if packages.len() == 0 {
+            break
+        }
+        for package in packages {
+            let name = get_package_data(&package, "name").unwrap();
+            let version = get_package_data(&package, "version").unwrap();
+            let description = get_package_data(&package, "description").unwrap();
+            let created = get_create_time(&package).unwrap();
+            println!("{} {} {}", name.green(), version, created.bright_black());
+            println!("  {}\n", description)
+        }
+        p += 1;
+    }
+    Ok(())
+}
